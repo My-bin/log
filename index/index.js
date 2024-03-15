623574d3cc1d7ba5a96c5b62b3762ff6cc43ebad
bin <2023605403@qq.com>
2024-03-15 11:13:23 +0800

    fix(优化重复调用溢出)

--- index/index.js
+++ index/index.js
@@ -1,25 +1,32 @@
 class log {
-    constructor(icon) {
-      this.icon = icon;
-    }
-    apply(compiler) {
-      const _that = this
-      const reg = /(console\.log\([^)]*\))/g;
-      compiler.hooks.emit.tap("log", (compilation) => {
-        Object.keys(compilation.assets).forEach((item) => {
-          let content = compilation.assets[item].source();
-          content = content.replace(reg, function (match, p1) {
+  constructor(icon) {
+    this.icon = icon;
+  }
+  apply(compiler) {
+    const _that = this;
+    const reg = /(console\.log\([^)]*\))/g;
+    compiler.hooks.emit.tap("log", (compilation) => {
+      Object.keys(compilation.assets).forEach((item) => {
+        let source = compilation.assets[item].source();
+        console.log(source, "!!!!!");
+        if (typeof source === 'function') {
+          source = source(); // 如果是函数，执行以获取字符串
+        }
+        if (typeof source === 'string') { // 检查source是否是字符串
+          source = source.replace(reg, function (match, p1) {
             console.log(match, "!!!!!");
             return match.slice(0, -1) + `,'${_that.icon}')`;
           });
           // 更新构建产物对象
           compilation.assets[item] = {
-            source: () => content,
-            size: () => content.length,
+            source: () => source, // 返回函数
+            size: () => source.length,
           };
-        });
+        } else {
+          console.error('Error: Compilation asset source is not a string.');
+        }
       });
-    }
+    });
   }
-  module.exports = log;
-  
+}
+module.exports = log;
