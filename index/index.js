f623a151ded157f2a8caa363505a59429947059e
bin <2023605403@qq.com>
2024-04-12 15:51:10 +0800

    fix:(修改打印字符串可能会造成的报错)

--- index/index.js
+++ index/index.js
@@ -4,18 +4,26 @@ class log {
   }
   apply(compiler) {
     const _that = this;
-    const reg = /(console\.log\([^)]*\))/g;
+    // 正则表达console.log()
+    const reg = /(console\.log\((?:[^)(]|\((?:[^)(]+|\([^)(]*\))*\))*\))/g;
     compiler.hooks.emit.tap("log", (compilation) => {
+      // 遍历所有的构建产物
       Object.keys(compilation.assets).forEach((item) => {
+        // 获取当前构建产物的源代码
         let source = compilation.assets[item].source();
+        // 如果是函数，执行以获取字符串
         if (typeof source === "function") {
-          source = source(); // 如果是函数，执行以获取字符串
+          source = source(); 
         }
+         // 检查source是否是字符串
         if (typeof source === "string") {
-          // 检查source是否是字符串
+          // 使用正则表达式替换源代码中的 console.log() 语句
           source = source.replace(reg, function (match, p1) {
-            return match.slice(0, -1) + `,'${_that.icon}')`
-            // return match.slice(0, -1) + `,'${_that.icon}')`;
+            const args = match.slice(12, -1) 
+            const newArgs = args.split(",").map(item => {
+              return _that.customLog(item)
+            }).join(',');
+            return `console.log(${newArgs},'${_that.icon}')`;
           });
           // 更新构建产物对象
           compilation.assets[item] = {
@@ -28,6 +36,13 @@ class log {
       });
     });
   }
-
+  customLog(variableValue) {
+    if(variableValue.includes("'") || variableValue.includes('"')){
+      return  variableValue
+    }else{
+      let str = variableValue.split('').map(item => item.toString()).join('');
+      return  `'${str}=',${variableValue}`
+    }
+  }
 }
 module.exports = log;
